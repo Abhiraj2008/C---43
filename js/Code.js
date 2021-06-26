271c5a3cf674604f4054bfe6752bfb3d4b0a7a47
Abhiraj2008 <76546576+Abhiraj2008@users.noreply.github.com>
2021-06-26 17:04:21 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "TAJMAHAL";
+const accessCode2 = "WASHINGTON";
+const accessCode3 = "MINECRAFT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("M T J H A L A A", 100,50)
+    fill("lightblue")
+    text("Hint: Pride of India !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("T W S A H N I 0 G N", 100,250)
+    fill("lightblue")
+    text("Hint: Very Famous Place !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("C M I R N A T F E", 700,150)
+    fill("lightblue")
+    text("Hint: A Famous Game !!", 700,170)
+
+}
